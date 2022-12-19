10192824d310b9999986307ad2508d720af549af
Diana Nguyen <diananguyen@Dianas-MBP-2.attlocal.net>
2022-12-18 18:10:18 -0800

    created apiroute.js and did js

---
+++ Develop/public/routes/apiRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+router.get('/', (req,res) => {
+    // get all the notes from DB
+    res.json(`GOT YOUR ${req.method} REQUEST`);
+});
+
+router.post('/', (req,res) => {
+    // add a note to the DB
+    res.json(`GOT YOUR ${req.method} REQUEST`);
+});
+
+module.exports = router;
---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+app.use(express.static('public'));
+
+app.use('/api/notes', apiRoutes);
+
+// API GET routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+// Listening
+app.listen(PORT, ()=> console.log(`Your app is running at http://localhost:${PORT}`));
